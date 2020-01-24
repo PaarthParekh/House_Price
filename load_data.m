99784f63f8a4eb5accfecb4e8d90bcb79f1f23ff
PaarthParekh <55130298+PaarthParekh@users.noreply.github.com>
2020-01-23 19:13:58 -0500

    Update load_data.m

--- load_data.m
+++ load_data.m
@@ -1,8 +1,7 @@
 function [X,y]= load_data()
-[file,folder]=uigetfile
-filename=fullfile(folder,file)
+[file,folder]=uigetfile;
+filename=fullfile(folder,file);
 data=load(filename);
-X=data(1:30,1:2);
-y=data(1:30,3)
-
-fprintf(' x = [%.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
+X=data(:,1:2);
+y=data(:,3);
+end
