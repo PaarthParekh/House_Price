1e52379f691ef3a340dce8f865fe8e4db397bfa8
PaarthParekh <55130298+PaarthParekh@users.noreply.github.com>
2020-01-23 19:04:13 -0500

    Update predict_house_price.m

--- predict_house_price.m
+++ predict_house_price.m
@@ -4,7 +4,7 @@ str = inputdlg('Enter size of the house and the number of bedrooms separated by 
 numbers = str2num(str{1});
 x=reshape(numbers,[1,2]);
 x=[1,x];
-x(2)=(x(2)-mu)/sigma
+x(2)=(x(2)-mu)/sigma;
 price = x*theta;
-fprintf('Predicted price=%f\n', price);
+fprintf('Predicted price=$%f\n', round(price,2));
 end
