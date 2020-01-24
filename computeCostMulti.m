ab9897ada0d5a757beec61ce8a38c9530e729d6e
PaarthParekh <55130298+PaarthParekh@users.noreply.github.com>
2020-01-23 19:33:37 -0500

    Update computeCostMulti.m

--- computeCostMulti.m
+++ computeCostMulti.m
@@ -9,16 +9,9 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
 
 predict = X*theta;
 s_e = (predict-y).^2;
 J=1/(2*m) * sum(s_e);
-
-
-
-% =========================================================================
 
 end
